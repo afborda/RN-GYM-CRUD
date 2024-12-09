45f460c70af5cea31bb23ad5dcc8c70f698f7c11
C1329353 Abner Borda Fonseca <c1329353@interno.bb.com.br>
2024-12-09 01:14:43 -0300

    Finalizado telas e inputs

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,31 +1,57 @@
-import { Input as GluestackInput, InputField } from "@gluestack-ui/themed";
+import {
+  Input as GluestackInput,
+  InputField,
+  FormControl,
+  FormControlError,
+  FormControlErrorText
+} from "@gluestack-ui/themed";
 import { ComponentProps } from "react";
 
 type Props = ComponentProps<typeof InputField> & {
   isReadOnly?: boolean;
+  erroMessage?: string | null;
+  isInvalid?: boolean;
 };
 
-export function Input({ isReadOnly = false, ...rest }: Props) {
+export function Input({
+  isReadOnly = false,
+  erroMessage = null,
+  isInvalid = false,
+  ...rest
+}: Props) {
+  const invalid = !!erroMessage || isInvalid;
   return (
-    <GluestackInput
-      h="$14"
-      borderWidth="$0"
-      borderRadius="$md"
-      $focus={{
-        borderWidth: 1,
-        borderColor: "$green500"
-      }}
-      isReadOnly={isReadOnly}
-      opacity={isReadOnly ? 0.5 : 1}
-    >
-      <InputField
-        px="$4"
-        bg="$gray700"
-        color="$white"
-        placeholderTextColor="$gray300"
-        fontFamily="$body"
-        {...rest}
-      />
-    </GluestackInput>
+    <FormControl isInvalid={invalid} mb="$2" w="$full">
+      <GluestackInput
+        h="$14"
+        isInvalid={invalid}
+        borderWidth="$0"
+        borderRadius="$md"
+        $focus={{
+          borderWidth: 1,
+          borderColor: invalid ? "$red500" : "$green500"
+        }}
+        $invalid={{
+          borderWidth: 1,
+          borderColor: "$red500"
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          px="$4"
+          bg="$gray700"
+          color="$white"
+          placeholderTextColor="$gray300"
+          fontFamily="$body"
+          {...rest}
+        />
+      </GluestackInput>
+      <FormControlError>
+        <FormControlErrorText color="$red500">
+          {erroMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
--- src/screens/signUp/index.tsx
+++ src/screens/signUp/index.tsx
@@ -1,3 +1,6 @@
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useNavigation } from "@react-navigation/native";
+import { useForm, Controller } from "react-hook-form";
 import {
   Center,
   Heading,
@@ -6,19 +9,74 @@ import {
   VStack,
   ScrollView
 } from "@gluestack-ui/themed";
-import BackgroundImg from "@assets/background.png";
-import Logo from "@assets/logo.svg";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+
+import BackgroundImg from "@assets/background.png";
+import Logo from "@assets/logo.svg";
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+};
+
+const signUpSchema = yup.object({
+  name: yup
+    .string()
+    .required("Nome é obrigatório")
+    .min(3, "Nome deve ter no mínimo 3 caracteres")
+    .max(80, "Nome deve ter no máximo 80 caracteres")
+    .matches(/^[a-zA-Z\s]+$/, "Nome deve conter apenas letras"),
+  email: yup
+    .string()
+    .required("E-mail é obrigatório")
+    .email("E-mail inválido")
+    .max(80, "E-mail deve ter no máximo 80 caracteres")
+    .min(5, "E-mail deve ter no mínimo 5 caracteres"),
+  password: yup
+    .string()
+    .required("Senha é obrigatória")
+    .min(6, "Senha deve ter no mínimo 6 caracteres")
+    .max(20, "Senha deve ter no máximo 20 caracteres")
+    .matches(
+      /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/,
+      "Senha deve conter pelo menos uma letra maiúscula, uma letra minúscula e um número"
+    ),
+  password_confirm: yup
+    .string()
+    .required("Confirmação de senha é obrigatória")
+    .oneOf([yup.ref("password"), null], "As senhas devem ser iguais")
+});
 
 export function SignUp() {
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: { errors }
+  } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
+
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
 
   const handleNavigateToSignIn = () => {
     navigation.navigate("signIn");
   };
+
+  function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm
+  }: FormDataProps) {
+    console.log({ name, email, password, password_confirm });
+  }
 
   return (
     <ScrollView
@@ -41,17 +99,71 @@ export function SignUp() {
             <Text color="$gray100">Treine sua mente o seu corpo.</Text>
           </Center>
 
-          <Center gap="$2" flex={1}>
+          <Center gap="$4" flex={1}>
             <Heading color="$gray100">Crie sua conta</Heading>
-            <Input placeholder="Nome" />
-            <Input
-              placeholder="E-mail"
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            <Input placeholder="Senha" secureTextEntry />
 
-            <Button title="Criar e acessar " />
+            <Controller
+              control={control}
+              name="name"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Nome"
+                  onChangeText={onChange}
+                  value={value}
+                  erroMessage={errors.name?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="email"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="E-mail"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  onChangeText={onChange}
+                  value={value}
+                  erroMessage={errors.email?.message}
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Senha"
+                  onChangeText={onChange}
+                  value={value}
+                  erroMessage={errors.password?.message}
+                  secureTextEntry
+                />
+              )}
+            />
+
+            <Controller
+              control={control}
+              name="password_confirm"
+              render={({ field: { onChange, value } }) => (
+                <Input
+                  placeholder="Confirme sua senha"
+                  onChangeText={onChange}
+                  value={value}
+                  onSubmitEditing={handleSubmit(handleSignUp)}
+                  erroMessage={errors.password_confirm?.message}
+                  returnKeyType="send"
+                  secureTextEntry
+                />
+              )}
+            />
+
+            <Button
+              title="Criar e acessar "
+              onPress={handleSubmit(handleSignUp)}
+            />
           </Center>
 
           <Button
